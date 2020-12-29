0e65f84276aeaafe63ce708f1ba74a4cb54fa928
shiba-jp <63838451+shiba-jp@users.noreply.github.com>
2020-12-29 15:42:21 +0900

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,88 @@
+def on_on_overlap(sprite, otherSprite):
+    otherSprite.destroy()
+    sprite.destroy(effects.fire, 100)
+    info.change_score_by(1)
+sprites.on_overlap(SpriteKind.projectile, SpriteKind.enemy, on_on_overlap)
+
+def on_on_overlap2(sprite, otherSprite):
+    otherSprite.destroy()
+    info.change_life_by(-1)
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap2)
+
+def on_a_pressed():
+    global dart
+    dart = sprites.create_projectile_from_sprite(img("""
+            . . 2 2 b b b b b . . . . . . . 
+                    . 2 b 4 4 4 4 4 4 b . . . . . . 
+                    2 2 4 4 4 4 d d 4 4 b . . . . . 
+                    2 b 4 4 4 4 4 4 d 4 b . . . . . 
+                    2 b 4 4 4 4 4 4 4 d 4 b . . . . 
+                    2 b 4 4 4 4 4 4 4 4 4 b . . . . 
+                    2 b 4 4 4 4 4 4 4 4 4 e . . . . 
+                    2 2 b 4 4 4 4 4 4 4 b e . . . . 
+                    . 2 b b b 4 4 4 b b b e . . . . 
+                    . . e b b b b b b b e e . . . . 
+                    . . . e e b 4 4 b e e e b . . . 
+                    . . . . . e e e e e e b d b b . 
+                    . . . . . . . . . . . b 1 1 1 b 
+                    . . . . . . . . . . . c 1 d d b 
+                    . . . . . . . . . . . c 1 b c . 
+                    . . . . . . . . . . . . c c . .
+        """),
+        spacePlane,
+        200,
+        50)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+bogey: Sprite = None
+dart: Sprite = None
+spacePlane: Sprite = None
+spacePlane = sprites.create(img("""
+        . . . . . . . . . . b 5 b . . . 
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . . . . b c . . . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . . . . b b 5 d 1 f 5 5 d f . . 
+            . . . . b 5 5 1 f f 5 d 4 c . . 
+            . . . . b 5 5 d f b d d 4 4 . . 
+            b d d d b b d 5 5 5 4 4 4 4 4 b 
+            b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+            b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+            c d d c d 5 5 b 5 5 5 5 5 5 b . 
+            c b d d c c b 5 5 5 5 5 5 5 b . 
+            . c d d d d d d 5 5 5 5 5 d b . 
+            . . c b d d d d d 5 5 5 b b . . 
+            . . . c c c c c c c c b b . . .
+    """),
+    SpriteKind.player)
+spacePlane.set_flag(SpriteFlag.STAY_IN_SCREEN, True)
+info.set_life(3)
+controller.move_sprite(spacePlane, 200, 200)
+
+def on_update_interval():
+    global bogey
+    bogey = sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . c c c c . . . . 
+                    . . . . . . c c d d d d c . . . 
+                    . . . . . c c c c c c d c . . . 
+                    . . . . c c 4 4 4 4 d c c . . . 
+                    . . . c 4 d 4 4 4 4 4 1 c . c c 
+                    . . c 4 4 4 1 4 4 4 4 d 1 c 4 c 
+                    . c 4 4 4 4 1 4 4 4 4 4 1 c 4 c 
+                    f 4 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+                    f 4 4 4 f 4 1 c c 4 4 4 1 f 4 f 
+                    f 4 4 4 4 4 1 4 4 f 4 4 d f 4 f 
+                    . f 4 4 4 4 1 c 4 f 4 d f f f f 
+                    . . f f 4 d 4 4 f f 4 c f c . . 
+                    . . . . f f 4 4 4 4 c d b c . . 
+                    . . . . . . f f f f d d d c . . 
+                    . . . . . . . . . . c c c . . .
+        """),
+        SpriteKind.enemy)
+    bogey.set_velocity(-100, 0)
+    bogey.left = scene.screen_width()
+    bogey.y = randint(0, scene.screen_height())
+    bogey.set_flag(SpriteFlag.AUTO_DESTROY, True)
+game.on_update_interval(500, on_update_interval)
---
+++ main.ts
@@ -0,0 +1,77 @@
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    sprite.destroy(effects.fire, 100)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeLifeBy(-1)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    dart = sprites.createProjectileFromSprite(img`
+        . . 2 2 b b b b b . . . . . . . 
+        . 2 b 4 4 4 4 4 4 b . . . . . . 
+        2 2 4 4 4 4 d d 4 4 b . . . . . 
+        2 b 4 4 4 4 4 4 d 4 b . . . . . 
+        2 b 4 4 4 4 4 4 4 d 4 b . . . . 
+        2 b 4 4 4 4 4 4 4 4 4 b . . . . 
+        2 b 4 4 4 4 4 4 4 4 4 e . . . . 
+        2 2 b 4 4 4 4 4 4 4 b e . . . . 
+        . 2 b b b 4 4 4 b b b e . . . . 
+        . . e b b b b b b b e e . . . . 
+        . . . e e b 4 4 b e e e b . . . 
+        . . . . . e e e e e e b d b b . 
+        . . . . . . . . . . . b 1 1 1 b 
+        . . . . . . . . . . . c 1 d d b 
+        . . . . . . . . . . . c 1 b c . 
+        . . . . . . . . . . . . c c . . 
+        `, spacePlane, 200, 50)
+})
+let bogey: Sprite = null
+let dart: Sprite = null
+let spacePlane: Sprite = null
+spacePlane = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b c . . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    b d d d b b d 5 5 5 4 4 4 4 4 b 
+    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+    c d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c b d d c c b 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+spacePlane.setFlag(SpriteFlag.StayInScreen, true)
+info.setLife(3)
+controller.moveSprite(spacePlane, 200, 200)
+game.onUpdateInterval(500, function () {
+    bogey = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . c c c c . . . . 
+        . . . . . . c c d d d d c . . . 
+        . . . . . c c c c c c d c . . . 
+        . . . . c c 4 4 4 4 d c c . . . 
+        . . . c 4 d 4 4 4 4 4 1 c . c c 
+        . . c 4 4 4 1 4 4 4 4 d 1 c 4 c 
+        . c 4 4 4 4 1 4 4 4 4 4 1 c 4 c 
+        f 4 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+        f 4 4 4 f 4 1 c c 4 4 4 1 f 4 f 
+        f 4 4 4 4 4 1 4 4 f 4 4 d f 4 f 
+        . f 4 4 4 4 1 c 4 f 4 d f f f f 
+        . . f f 4 d 4 4 f f 4 c f c . . 
+        . . . . f f 4 4 4 4 c d b c . . 
+        . . . . . . f f f f d d d c . . 
+        . . . . . . . . . . c c c . . . 
+        `, SpriteKind.Enemy)
+    bogey.setVelocity(-100, 0)
+    bogey.left = scene.screenWidth()
+    bogey.y = randint(0, scene.screenHeight())
+    bogey.setFlag(SpriteFlag.AutoDestroy, true)
+})
